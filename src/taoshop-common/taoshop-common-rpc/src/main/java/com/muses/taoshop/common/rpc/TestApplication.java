8b594b7a48fbd152dba2805b16e84baf27d10e91
nicky <362330721@qq.com>
2018-11-18 21:26:58 +0800

    加上RPC工程

---
+++ src/taoshop-common/taoshop-common-rpc/src/main/java/com/muses/taoshop/common/rpc/TestApplication.java
@@ -0,0 +1,16 @@
+package com.muses.taoshop.common.rpc;
+
+/**
+ * <pre>
+ *  测试类
+ * </pre>
+ *
+ * @author nicky
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2018.11.17 22:20    修改内容:
+ * </pre>
+ */
+public class TestApplication {
+}
